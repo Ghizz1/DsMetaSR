5b7baf1148b8e95140bdee2aac27cda3b33e7016
Ghizz1 <m_ghizzi@hotmail.com>
2022-07-14 17:45:34 -0300

    começo react

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
+import NotificationButton from "./assets/components/NotificationButton"
 
 function App() {
   return (
-    <h1>
-      "Ola mundo"
-    </h1>
+    <>
+      <h1>Ola mundo</h1>
+      <NotificationButton />
+    </>
   )
 }
 
